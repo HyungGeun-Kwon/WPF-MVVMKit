56b18f00ba9e8721ebabe69899a0314ea0cbf4f8
형근 권 <hgkwon@crevis.co.kr>
2025-04-04 10:33:43 +0900

    DialogService Register 시 직접 string 지정 가능

--- MVVMKit/Dialogs/IDialogService.cs
+++ MVVMKit/Dialogs/IDialogService.cs
@@ -5,6 +5,7 @@ namespace MVVMKit.Dialogs
     public interface IDialogService
     {
         void Register<TView>() where TView : Window;
+        void Register<TView>(string viewName) where TView : Window;
         bool ShowDialog(string viewName);
         void Show(string viewName);
     }
